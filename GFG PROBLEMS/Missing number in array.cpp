36d6bf2183882091fa8c7d50d012fb6d0fd806b2
Sanika Chaudhari <sanikachaudhari201003@gmail.com>
2023-09-17 18:14:25 +0530

    done

--- GFG PROBLEMS/Missing number in array.cpp
+++
@@ -1,9 +0,0 @@
- int missingNumber(vector<int>& array, int n) {
-        // Your code goes here
-        int sum=0;
-        sum = n*(n+1)/2;
-        for(int i=0; i<n-1; i++){
-            sum -= array[i];
-        }
-        return sum;
-    }
