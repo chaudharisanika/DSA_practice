20ff96e837dc08cba6249b435868a3697cf5ffab
chaudharisanika <113428174+chaudharisanika@users.noreply.github.com>
2023-09-17 18:22:51 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Reading books/reading-books.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    long long maxPoint(int N, int K, long long A[], long long B[])
+    {
+        // code here
+        long mult=0;
+        long maximum=0;
+        for(int i=0;i<N;i++)
+        {
+            long timing=K/A[i];
+            mult=timing*B[i];
+            
+            if(maximum<mult)
+            {
+                maximum=mult;
+            }
+        }
+        return maximum;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N, K;
+        cin>>N>>K;
+        long long A[N], B[N];
+        for(int i = 0;i < N;i++)
+            cin>>A[i];
+        for(int i = 0;i < N;i++)
+            cin>>B[i];
+        
+        Solution ob;
+        cout<<ob.maxPoint(N, K, A, B)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
